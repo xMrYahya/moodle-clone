3813e59694090fb77c187bb045c5e5b96ccf87d2
xMrYahya <ardyyahya@outlook.com>
2026-02-23 03:35:42 -0500

    Création des sous-classes concrètes

--- src/types/questionTypes.ts
+++ src/types/questionTypes.ts
@@ -10,6 +10,101 @@ export abstract class Question {
   abstract obtenirType(): string;
 }
 
+export class QuestionVraiFauxModele extends Question {
+  constructor(
+    nom: string,
+    énoncé: string,
+    retroactionValide: string,
+    retroactionInvalide: string,
+    tags: string[],
+    public reponse: boolean,
+    public retroaction: string
+  ) {
+    super(nom, énoncé, retroactionValide, retroactionInvalide, tags);
+  }
+
+  obtenirType(): string {
+    return "VraiFaux";
+  }
+}
+
+export class QuestionChoixMultipleModele extends Question {
+  constructor(
+    nom: string,
+    énoncé: string,
+    retroactionValide: string,
+    retroactionInvalide: string,
+    tags: string[],
+    public seulementUnChoix: boolean,
+    public reponses: ReponseChoixMultiple[]
+  ) {
+    super(nom, énoncé, retroactionValide, retroactionInvalide, tags);
+  }
+
+  obtenirType(): string {
+    return "ChoixMultiple";
+  }
+}
+
+export class QuestionMiseEnCorrespondanceModele extends Question {
+  constructor(
+    nom: string,
+    énoncé: string,
+    retroactionValide: string,
+    retroactionInvalide: string,
+    tags: string[],
+    public paires: PairDeCorrespondance[]
+  ) {
+    super(nom, énoncé, retroactionValide, retroactionInvalide, tags);
+  }
+
+  obtenirType(): string {
+    return "MiseEnCorrespondance";
+  }
+}
+
+export class QuestionReponseCourteModele extends Question {
+  constructor(
+    nom: string,
+    énoncé: string,
+    retroactionValide: string,
+    retroactionInvalide: string,
+    tags: string[],
+    public reponseAttendue: string,
+    public retroaction: string
+  ) {
+    super(nom, énoncé, retroactionValide, retroactionInvalide, tags);
+  }
+
+  obtenirType(): string {
+    return "ReponseCourte";
+  }
+}
+
+export class QuestionNumeriqueModele extends Question {
+  constructor(
+    nom: string,
+    énoncé: string,
+    retroactionValide: string,
+    retroactionInvalide: string,
+    tags: string[],
+    public reponseAttendue: number,
+    public retroaction: string
+  ) {
+    super(nom, énoncé, retroactionValide, retroactionInvalide, tags);
+  }
+
+  obtenirType(): string {
+    return "Numerique";
+  }
+}
+
+export class QuestionEssaiModele extends Question {
+  obtenirType(): string {
+    return "Essai";
+  }
+}
+
 export type DonneesQuestion = {
   nom: string;
   énoncé: string;
